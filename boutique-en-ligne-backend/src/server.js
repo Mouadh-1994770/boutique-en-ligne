ff78e8c85c0b0aec13b338db0b3c547335c63e8a
Mouadh-1994770 <1994770@csfoy.ca>
2021-06-03 11:24:00 -0400

    Ajout d'un nouveau produit

--- boutique-en-ligne-backend/src/server.js
+++ boutique-en-ligne-backend/src/server.js
@@ -28,5 +28,36 @@ app.get('/api/produits', (requete, reponse) => {
     );;
 });
 
+app.put('/api/produit/ajouter', (requete, reponse) => {
+    const { nom, description, categorie, prix, rabais, quantite } = requete.body;
+
+    if (nom !== undefined && description !== undefined && categorie !== undefined && prix > 0 &&
+        ((rabais >= 0) && (rabais < 100)) && quantite >= 0) {
+        utiliserDB(async (db) => {
+            await db.collection('pieces').insertOne({
+                nom: nom,
+                description: description,
+                categorie: categorie,
+                prix: prix,
+                rabais: rabais,
+                quantite: quantite
+            });
+
+            reponse.status(200).send("Produit ajouté");
+        }, reponse).catch(
+            () => reponse.status(500).send("Erreur : la produit n'a pas été ajouté")
+        );
+    }
+    else {
+        reponse.status(500).send(`Certains paramètres ne sont pas définis :
+            - nom: ${nom}
+            - description: ${description}
+            - categorie: ${categorie}
+            - prix: ${prix}
+            - rabais: ${rabais}
+            - quantite: ${quantite}`);
+    }
+});
+
 
 app.listen(8000, () => console.log('Écoute le port 8000'));
