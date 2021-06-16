8c45a7be9b46d2d56f1edf65c91247571b34435f
Hassnia <hasnia.deyah@gmail.com>
2021-06-16 02:27:24 -0400

    Ajout de la composant NombreProduitParPage

---
+++ boutique-en-ligne-frontend/src/Composants/SectionClient/NombreProduitParPublication.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NombreProduitParPublication() {
+    return (
+        <>
+            <div className="col-md-4">Sélectionner le nombre des produits par page </div>
+            <div>
+                <select onChange={props.handleChange}>
+                    <option selected="selected" label="5" value={12} />
+                    <option label="10" value={24} />
+                    <option label="12" value={48} />
+                    <option label="tous" value={props.longueurList} />
+                </select>
+            </div> 
+        </>
+    )
+}export default NombreProduitParPublication
