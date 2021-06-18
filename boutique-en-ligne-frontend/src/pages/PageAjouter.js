cc9df57927077394c8f8a1884c3b1c307edc4846
Mouadh-1994770 <1994770@csfoy.ca>
2021-06-18 16:17:14 -0400

    Ajout de la page Ajouter

---
+++ boutique-en-ligne-frontend/src/pages/PageAjouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FormulaireAjouterProduit from '../composants/FormulaireAjouterProduit';
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom';
+
+function PageAjouter() {
+    return (
+        <>
+            <h1>Ajouter un nouveau Produit</h1>
+            <FormulaireAjouterProduit />
+            <Link to="/admin">
+                <Button variant={'danger'} >Annuler</Button>
+            </Link>
+        </>
+    );
+}
+
+export default PageAjouter;
