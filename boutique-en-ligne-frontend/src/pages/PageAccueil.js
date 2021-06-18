f190481bba50768896dc4c34803cd7855415d593
Mouadh-1994770 <1994770@csfoy.ca>
2021-06-18 16:25:59 -0400

    Ajout de la page d'accueil

---
+++ boutique-en-ligne-frontend/src/pages/PageAccueil.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function PageAccueil() {
+    return (
+    <>
+        <h1>Bienvenue au BigShop</h1>
+        <p>Contents de vous Accueillir sur notre site ! Chez nous vous allez trouvez tous vos besoins pour une épicerie saine 
+            et diversifié ! Trouvez aussi tous  ce dont vous auriez besoin pour vos activités ménagères quotidiennes
+            à des prix imbattables ! Et en plus, on vous rembourse la différence si vous trouvez moins cher ailleurs !
+            Veuillez suivre nos circulaire hebdomadaires pour trouvez nos produits à rabais !
+        </p>
+        
+    </>
+    );
+}
+
+export default PageAccueil;
