36eafed94d632ed5f6d8fd88bb206f808f2173e3
ThierryNews <1995159@csfoy.ca>
2021-06-20 01:46:50 -0400

    Mise a jour de certains élements dans la page de connection pouvant verifier la présence ou nom d'un utilisateur

--- boutique-en-ligne-frontend/src/Composants/SectionUtilisateur/PageConnexion.js
+++ boutique-en-ligne-frontend/src/Composants/SectionUtilisateur/PageConnexion.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
+import pageValidation from "./PageValidation";
+import {Redirect} from 'react-router-dom';
 import "./Page.css";
-import PageValidation from "./PageValidation";
 import Button from 'react-bootstrap/Button';
-import {Redirect} from 'react-router-dom'
 
 
-function PageConnexion({ error }) {
-    const [values, setValues] = useState({nom: "", password: "" });
-    const [errors, setErrors] = useState();
+function PageConnexion() {
+    const [values, setValues] = useState({nom: "", password: ""});
+    const [errors, setErrors] = useState({});
     const [dataIsCorrect, setDataIsCorrect] = useState(false);
     const [rediriger, setRediriger] = useState(false); 
+    const [role, setRole] = useState("");
 
     const handleChange = (event) => {
         setValues({
@@ -17,9 +18,10 @@ function PageConnexion({ error }) {
             [event.target.name]: event.target.value,
         });
     };
-    const handlerFromSubmit = (event) => {
+
+    const envoyerFormulaire = (event) => {
         event.preventDefault();
-        let errors = PageValidation(values, true);
+        let errors = pageValidation(values);
         setErrors(errors);
         setDataIsCorrect(!errors.hasError);
     }
@@ -28,42 +30,59 @@ function PageConnexion({ error }) {
         if(dataIsCorrect){
             let utilisateur = {
                 nom:values.nom, 
-                password:values.password,       
+                password:values.password   
             }
-            verifierUtilisateur(utilisateur);
+            verifierUtilisateur(utilisateur)
+            .then(utilisateurExistant =>{  
+                if(utilisateurExistant){
+                    if(utilisateur.nom ==="admin" && utilisateur.password ==="admin"){
+                        setRole("Administrateur") 
+                    }else{
+                        setRole("Client")             
+                    }
+                    setRediriger(true);
+                }
+            });
         }      
         setDataIsCorrect(false)
 
     }, [dataIsCorrect]);
 
     const verifierUtilisateur = async (utilisateur) => {
-        const resultat = await fetch(`/api/utilisateur/existe${utilisateur.nom}/${utilisateur.password}`);
-        setRediriger(true);    
+        const resultat = await fetch(`/api/utilisateur/existe`, {
+            method: 'post',
+            body: JSON.stringify(utilisateur),
+            headers: {
+                'Content-Type': 'application/json'
+            }               
+        });
+        return resultat.status === 200;
     };
 
     function AfficherRedirection() {
-        if(values.role ==="Admin" && rediriger === true){
-            return <Redirect to="/Admin" />
-        }
-        else if(values.role ==="Client" && rediriger === true){
-            return <Redirect to= "/Client"/>
-        };
+        if(rediriger === true){
+            if(role ==="Administrateur"){
+                return <Redirect to="/Administrateur" />
+            }
+            else{                
+                return <Redirect to= "/Client"/>
+            };
+        } 
     }
-
+    
     return (
-        <>  {AfficherRedirection}   
+        <>   {AfficherRedirection()} 
             <div className="name">
-                <input className="input" placeholder="Nom utilisateur" type="text" name="nom" id="nom" onChange={handleChange} value={values.nom} />
+                <input className = "input" placeholder = "Nom utilisateur" type="text" name = "nom" id = "nom" onChange={handleChange} value={values.nom}/>
                 {errors.nom && <p className="error">{errors.nom}</p>}
             </div>
             <div className="password">  
-                <input className="input" placeholder="Mot de passe" type="password" name="password" id="password" onChange={handleChange } value={values.password} />
+                <input className = "input" placeholder = "Mot de passe" type = "password" name = "password" id = "password" onChange={handleChange } value={values.password} />
                 {errors.password && <p className="error">{errors.password}</p>}
             </div>
-            <Button className="mt-2" variant="outline-success" style={{ fontWeight: "bold" }} onClick={handlerFromSubmit} >
+            <Button className="mt-2" variant="outline-success"  id ="connecter" style={{ fontWeight: "bold" }} onClick={envoyerFormulaire} >
             Se connecter
-            </Button>
-            {(error !== "") ? (<div className="erro">{error}</div>) : ""}
+            </Button>           
         </>
     );
 };
